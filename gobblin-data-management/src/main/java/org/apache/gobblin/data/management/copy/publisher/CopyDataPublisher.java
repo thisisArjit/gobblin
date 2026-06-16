2b83227736d1322b748377a9de8567c5c0146814
arjit3251 <thisisarjit@gmail.com>
2026-06-16 12:30:25 +0530

    Propagate job properties to Work units

--- gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/publisher/CopyDataPublisher.java
+++ gobblin-data-management/src/main/java/org/apache/gobblin/data/management/copy/publisher/CopyDataPublisher.java
@@ -342,9 +342,7 @@ public class CopyDataPublisher extends DataPublisher implements UnpublishedHandl
 
     if (statesHelper.hasAnyCopyableFile()) {
       // Targets are always absolute, so we start moving from root (will skip any existing directories).
-      // Rename data files before metadata files so Iceberg metadata is never published ahead of the data it references.
-      HadoopUtils.renameRecursivelyOrdered(this.fs, datasetWriterOutputPath, new Path("/"),
-          HadoopUtils::icebergRenamePriority);
+      HadoopUtils.renameRecursively(this.fs, datasetWriterOutputPath, new Path("/"));
     } else {
       log.info("[{}] No copyable files in dataset. Proceeding to post-publish steps.", datasetAndPartition.identifier());
     }
